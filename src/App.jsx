e7d94892dd51c1b83e71ad5ac57174dbabaecdad
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-10-03 15:08:01 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,41 @@
 import {useState , useEffect} from "react"
 import Start from "./components/start"
+import Question from "./components/question";
 // import Question from "./components/question";
 
 export const App = () => {
 
   const [startVis, setStartVis] = useState(true);
 
-  const [allData , setAllData] = useState(null)
+  const [allData , setAllData] = useState([])
   
-  const [answers,setAnswers] = useState([])
+    // shuffle function to change place of correct answer
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+      }
+      return array;
+    }
 
+    const handleAnswered = (answer) => {
+      
+
+    }
 
     //pulls from api and aranges data into predifined states 
   useEffect(()=>{
     fetch("https://the-trivia-api.com/api/questions ")
     .then(res => res.json())
     .then((data) => {
-      setAllData(data.splice(5))
-      const allAnswers = data.map(res => ([...res.incorrectAnswers, res.correctAnswer]))
-      const final = allAnswers.map(element => 
-        element.map((ans,index) => ({
-          answer : ans,
-          chosen:false,
-          isCorrect : index === element.length - 1  // not ideal solution but it will do for now ;)
-        }))
-      );
-      final.forEach(array => {
-        shuffleArray(array)
-      });
-      setAnswers(final)
+      setAllData(data.splice(5).map((q => ({
+        ...q ,
+        allAnswers : shuffleArray([...q.incorrectAnswers, q.correctAnswer]),
+        isAnswered : false,
+        isAnsweredCorrectly : false
+      }))))
     })
   },[])
-
-  // shuffle function to change place of correct answer
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
 
 
 const hide = () => {
@@ -47,9 +44,23 @@ const hide = () => {
 }
 
 
+const qElements = allData.map(q => (
+  <Question
+      key={q.id}
+      id={q.id}
+      question={q.question}
+      allAnswers = {q.allAnswers}
+      correct = {q.correctAnswer}
+      isAnswered = {q.isAnswered}
+
+  />
+)) 
+
 return (
   <div>
     {startVis && <Start hide={hide}/>}
+    {!startVis &&  qElements }
+    {!startVis && <button>Check answers</button>}
   </div>
 )
 }
--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,11 +1,26 @@
-// const AnswersComp = (props) => {
-//     return (
-//         <div>
-//             <h1>{props.map(prop => {
+import { useEffect, useState } from "react";
 
-//             })}</h1>
-//         </div>
-//     )
-// }
+const Question = (props) => {
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-// export default AnswersComp
+  const handleRadioChange = (event) => {
+    setSelectedAnswer(event.target.value);
+  };
+
+  useEffect(()=>{
+    console.log(selectedAnswer)
+  },[selectedAnswer])
+
+    return (
+        <div className="w-8/12 mx-auto m-4 pb-4 border-b border-gray-200">
+            <h1 className="font-bold py-4">{props.question}</h1>
+            <div>
+            {props.allAnswers.map(answer => (
+               <label className={selectedAnswer === answer? "bg-blue-300 mx-2 p-1 rounded-lg text-xs" : "bg-gray-200 mx-2 p-1 rounded-lg text-xs" }>{answer}<input type="radio" name={props.id} value={answer}checked={selectedAnswer === answer} onChange={handleRadioChange}/></label>
+            ))}
+            </div>
+        </div>
+    )
+}
+
+export default Question ; 
